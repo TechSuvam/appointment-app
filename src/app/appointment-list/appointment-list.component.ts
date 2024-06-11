0218fcac993f2ed0e35cd19558b50fd03575938f
Suvam Chanda <Suvam_Chanda@epam.com>
2024-06-11 15:19:19 +0530

    Add local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+   
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []; 
+  }
+
   newAppointmentTitle: string = '';
   newAppointmentDate: Date = new Date();
   appointments: Appointment[] = [];
@@ -22,10 +30,13 @@ export class AppointmentListComponent {
       this.appointments.push(newAppointment);
       this.newAppointmentTitle = "";
       this.newAppointmentDate = new Date();
+
+      localStorage.setItem("appointments", JSON.stringify(this.appointments));
      }
   }
 
   deleteAppointment(index : number){
     this.appointments.splice(index, 1);
+    localStorage.setItem("appointments", JSON.stringify(this.appointments));
   }
 }
